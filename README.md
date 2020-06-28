81444bf08575727a19d8d4b5e82d7424a21ff862
chrisloarryn <chrisloarryn@gmail.com>
2020-06-28 03:11:56 -0400

    feat: find by age, firstName and by Id

---
+++ README.md
@@ -0,0 +1,13 @@
+{
+	user(id: "23") {
+	  id,
+    firstName
+	}
+}
+
+{
+	user(age: 21) {
+	  id,
+    firstName
+	}
+}
--- schema/schema.js
+++ schema/schema.js
@@ -1,18 +1,33 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQlString, GraphQlInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '47', firstName: 'Samantha', age: 21 },
+];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
-    id: {
-      type: GraphQlString,
-    },
-    firstName: {
-      type: GraphQlString,
-    },
-    age: {
-      type: GraphQlInt,
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString }, firstName: { type: GraphQLString }, age: { type: GraphQLInt } },
+      resolve(parentValue, args) {
+        return _.find(users, { ...args });
+      },
     },
   },
 });
+
+module.exports = new GraphQLSchema({ query: RootQuery });
